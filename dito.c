5e9e995cdb8da40a57bb53a4d55983257e7a693c
Emily <emily.moraima.cruz@hotmail.com>
2024-03-09 02:04:33 -0400

    Comments and ICANON added

--- dito.c
+++ dito.c
@@ -1,19 +1,20 @@
-#include <stdlib.h>
-#include <termios.h>
-#include <unistd.h>
+#include <stdlib.h> // para atexits
+#include <termios.h> // funciones y mas para controlar el terminal.
+#include <unistd.h> // funciones de manejo descriptores de archivos
 
 
-struct termios orig_termios; // Guarda la configuración inicial del terminal :v
+struct termios orig_termios; // Definir una estructura termios
 
-void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+void disableRawMode() { // Esta funcion restaura la configuracion original del terminal :)
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); // restaura la config inicial que está dentro de orig_termios
 }
+
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
-  atexit(disableRawMode);
-  struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO);
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  tcgetattr(STDIN_FILENO, &orig_termios); // obtiene la config inicial del terminal y lo guarda en orig_termios
+  atexit(disableRawMode); // registra la funcion disableRawMode para que la llame al final del programa
+  struct termios raw = orig_termios; // Crea una copia de la config original obvi
+  raw.c_lflag &= ~(ECHO | ICANON); // desactiva el eco, o sea que lo que uno escriba no se verá :)
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // aplica la config modificada a la terminal
 }
 
 int main() {
