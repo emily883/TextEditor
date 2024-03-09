87e169dd89501b8a9c824ea3ced2ecc83d9a2280
Emily <emily.moraima.cruz@hotmail.com>
2024-03-09 01:22:54 -0400

    DisableRawMode function added

--- dito.c
+++ dito.c
@@ -1,23 +1,24 @@
-#include <unistd.h>
-#include <termios.h>
 #include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
 
-struct termios orig_termios;
+
+struct termios orig_termios; // Guarda la configuración inicial del terminal :v
+
 void disableRawMode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
-
 void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_lflag &= ~(ECHO);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-int main(){
-    enableRawMode();
-    char c;
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
-    return 0;
+int main() {
+  enableRawMode();
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  return 0;
 }
