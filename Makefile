afc90f844c3475b7fafbba885a41c76386cfa416
Emily <emily.moraima.cruz@hotmail.com>
2024-03-17 01:25:49 -0400

    Pushing let's gooo

--- Makefile
+++ Makefile
@@ -1,1 +1,2 @@
 dito: dito.c
+	$(CC) dito.c -o dito -Wall -Wextra -pedantic  -std=c99
--- dito.c
+++ dito.c
@@ -1,6 +1,8 @@
 #include <stdlib.h> // para atexits
 #include <termios.h> // funciones y mas para controlar el terminal.
 #include <unistd.h> // funciones de manejo descriptores de archivos
+#include <ctype.h> // pa saber q caracter es, si es numero o es letra mayuscula o minuscula y tmbn convierte mayus a minus y cositas asi :v
+#include <stdio.h> // para entrada y salida estandar uwu
 
 
 struct termios orig_termios; // Definir una estructura termios
@@ -10,11 +12,12 @@ void disableRawMode() { // Esta funcion restaura la configuracion original del t
 }
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios); // obtiene la config inicial del terminal y lo guarda en orig_termios
-  atexit(disableRawMode); // registra la funcion disableRawMode para que la llame al final del programa
-  struct termios raw = orig_termios; // Crea una copia de la config original obvi
-  raw.c_lflag &= ~(ECHO | ICANON); // desactiva el eco, o sea que lo que uno escriba no se verá :)
+  tcgetattr(STDIN_FILENO, &orig_termios); // obtiene la config actual del terminal y lo guarda en orig_termios. tcgetattr() obtienes los atributos del terminal
+  atexit(disableRawMode); // registra la funcion disableRawMode para que la llame al final del programa sin importar que
+  struct termios raw = orig_termios; // Crea una copia de la config original obvi, para asi poder modificar la copia sin dañar el original :)
+  raw.c_lflag &= ~(ECHO | ICANON); // desactiva el eco, o sea que lo que uno escriba no se verá :), el ICANON al desactivarlo la entrada se procesa al momento en q se escribe sin presionar enter.
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // aplica la config modificada a la terminal
+// el TCSAFLUSH asegura q el buffer de entrada y salida se limpien antes de aplicar los cambios en la configuracion del terminal :)
 }
 
 int main() {
