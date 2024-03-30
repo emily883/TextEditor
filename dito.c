8b1c69bd1724272bffb25948383b52b706b12f98
Emily <emily.moraima.cruz@hotmail.com>
2024-03-30 01:49:56 -0400

    sdf

--- dito.c
+++ dito.c
@@ -112,27 +112,55 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/*** append buffer ***/
+
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
+
+
 /*** output ***/
 
 
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-      write(STDOUT_FILENO, "~", 1);
+      abAppend(ab, "~", 1);
       if (y < E.screenrows - 1) {
-          write(STDOUT_FILENO, "\r\n", 2);
+          abAppend(ab, "\r\n", 2);
       }
   }
 }
 
 void editorRefreshScreen() {
-  write(STDOUT_FILENO, "\x1b[2J", 4); // escribir 4 bits, el \x1b es el caracter de escape es un byte, [2J el resto de bytes
-  write(STDOUT_FILENO, "\x1b[H", 3); // pone el cursor de texto al inicio, el comando [ H acepta dos parametros
+  struct abuf ab = ABUF_INIT;
+//  write(STDOUT_FILENO, "\x1b[2J", 4); // escribir 4 bits, el \x1b es el caracter de escape es un byte, [2J el resto de bytes
+//  write(STDOUT_FILENO, "\x1b[H", 3); // pone el cursor de texto al inicio, el comando [ H acepta dos parametros
+    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[H", 3);
 
-  editorDrawRows();
+//  editorDrawRows();
 
-  write(STDOUT_FILENO, "\x1b[H", 3);
+//  write(STDOUT_FILENO, "\x1b[H", 3);
 
+    editorDrawRows(&ab);
+    abAppend(&ab, "\x1b[H", 3);
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
 
 /*** input ***/
